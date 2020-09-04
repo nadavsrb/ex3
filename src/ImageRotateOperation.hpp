6561e3a1d8b6b6f8da47600005b42865e511119a
Nadavsrb <nadavsrb@gmail.com>
2020-09-04 15:16:31 +0300

    Fixing ImageRotateOperation to support the conventions

--- src/ImageRotateOperation.hpp
+++ src/ImageRotateOperation.hpp
@@ -4,8 +4,18 @@
 #include "Operation.hpp"
 #include "bmp_tester.hpp"
 
+/**
+ * @brief This class stands for the "image rotate" operation.
+ */
 class ImageRotateOperation : public Operation {
 public:
+    /**
+    * @brief Construct a new Image Rotate Operation object.
+    * 
+    * @param argc the lenght of the input (argv).
+    * @param argv includes the input files (& the output file if not from searched operation).
+    * @param isSearched true if the originall operation was search,else false.
+    */
     ImageRotateOperation(const int argc, const char *argv[], bool isSearched = false);
     
     string getOutputFileType() const override;
