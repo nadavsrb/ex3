1bef64e02e1e595f7069085d4b0baad67b2dba61
Nadavsrb <nadavsrb@gmail.com>
2020-09-04 16:53:30 +0300

    Fixing CurrentTime to support the conventions

--- src/CurrentTime.cpp
+++ src/CurrentTime.cpp
@@ -1,5 +1,17 @@
 #include "CurrentTime.hpp"
 
+using std::string;
+using std::uint32_t;
+
+/**
+ * @brief Get the String object.
+ * 
+ * note: excepected to get one or to digits number
+ * and to return string with to digits (1 -> 01);
+ * 
+ * @param num the number to get string from.
+ * @return string 
+ */
 string getString(int num) {
     string s = std::to_string(num);
 
@@ -11,12 +23,12 @@ string getString(int num) {
 }
 
 CurrentTime::CurrentTime(){
-    // current date/time based on current system
+    //current date/time based on current system
    time_t now = time(0);
 
    tm *ltm = localtime(&now);
 
-
+    //calculating the time & date to string
    _time += getString(ltm->tm_mday);
    _time += "/";
    _time += getString(ltm->tm_mon + 1);
@@ -28,6 +40,4 @@ CurrentTime::CurrentTime(){
    _time += getString(ltm->tm_min);
 }
 
-string CurrentTime::getTime() const{
-    return _time;
-}
+string CurrentTime::getTime() const{ return _time; }
--- src/CurrentTime.hpp
+++ src/CurrentTime.hpp
@@ -4,15 +4,26 @@
 #include <ctime>
 #include <stdint.h>
 
-using std::string;
-using std::uint32_t;
-
+/**
+ * @brief this class is for calculating the corent time.
+ */
 class CurrentTime {
-	string _time = "";
-
 public:
-    static const uint32_t TIME_STRING_LENGTH = 14;
+    static constexpr std::uint32_t TIME_STRING_LENGTH = 14;
 
+    /**
+     * @brief Construct a new Current Time object.
+     * 
+     */
     CurrentTime();
-    string getTime() const;
+
+    /**
+     * @brief Get the Time object.
+     * 
+     * @return std::string rturn string that
+     * stands for the time & date.
+     */
+    std::string getTime() const;
+private:
+    std::string _time = "";
 };
