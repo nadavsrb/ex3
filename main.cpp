f0fdd6ae3a22b91bb6f64a7ddc168b6712a7f4cd
tseela <tseelagamzo@gmail.com>
2020-09-04 19:01:00 +0300

    Solved casting problem without the stupid cast.

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc,const char *argv[]) {
     // first arg is ex3.out and we don't need it
     int startIndex = 1;
     --argc;
 
     unique_ptr<Operation> operation;
-    bool isSearch = CacheManager::isSearch(argc, (const char**) argv + startIndex);
-    bool isClear = CacheManager::isClear(argc, (const char**) argv + startIndex);
+    bool isSearch = CacheManager::isSearch(argc, argv + startIndex);
+    bool isClear = CacheManager::isClear(argc, argv + startIndex);
 
     if (isSearch) {
         // first two words are "cache search", moving on to the operation
@@ -40,21 +40,21 @@ int main(int argc, char *argv[]) {
             // matrix operations
             if (strcmp(argv[startIndex], "matrix") == 0) {
                 if (strcmp(argv[startIndex + 1], "multiply") == 0) { // matrix multiply
-                    operation =  make_unique<MatrixMultOperation>(argc - 2,  (const char**) &argv[startIndex + 2], isSearch);
+                    operation =  make_unique<MatrixMultOperation>(argc - 2, &argv[startIndex + 2], isSearch);
                 } else if (strcmp(argv[startIndex + 1], "add") == 0) { // matrix add
-                    operation =  make_unique<MatrixAddOperation>(argc - 2, (const char**)  &argv[startIndex + 2], isSearch);
+                    operation =  make_unique<MatrixAddOperation>(argc - 2, &argv[startIndex + 2], isSearch);
                 }
             // hash operations
             } else if (strcmp(argv[startIndex], "hash") == 0) {
                 if (strcmp(argv[startIndex + 1], "crc32") == 0) { // hash crc32
-                    operation =  make_unique<HashCrc32Operation>(argc - 2,(const char**)  &argv[startIndex + 2], isSearch);
+                    operation =  make_unique<HashCrc32Operation>(argc - 2, &argv[startIndex + 2], isSearch);
                 }
             // image operations
             } else if (strcmp(argv[startIndex], "image") == 0) {
                 if (strcmp(argv[startIndex + 1], "rotate") == 0) { // image rotate
-                    operation =  make_unique<ImageRotateOperation>(argc - 2, (const char**) &argv[startIndex + 2], isSearch);
+                    operation =  make_unique<ImageRotateOperation>(argc - 2, &argv[startIndex + 2], isSearch);
                 } else if (strcmp(argv[startIndex + 1], "convert") == 0) { // image convert
-                    operation =  make_unique<ImageConvertOperation>(argc - 2, (const char**) &argv[startIndex + 2], isSearch);
+                    operation =  make_unique<ImageConvertOperation>(argc - 2, &argv[startIndex + 2], isSearch);
                 }
             // an operation that doesn't exists
             } else {
