831a77115551bb1729d65f0ebbbd55c5ad2d4b7a
Nadavsrb <nadavsrb@gmail.com>
2020-09-04 15:37:47 +0300

    Fixing ImageConvertOperation to support the conventions

--- src/ImageConvertOperation.cpp
+++ src/ImageConvertOperation.cpp
@@ -4,31 +4,34 @@ using namespace OperatorsHelpingFuncs;
 using namespace std;
 
 ImageConvertOperation::ImageConvertOperation(const int argc, const char *argv[], bool isSearched /*= false*/){
+    //fixing the expected args if the operation is from search.
     int argsExpected = argc;
     if (isSearched) {
         argsExpected++;
     }
 
-    if (argsExpected != 2) {
+    //exceptions
+    if (argsExpected != 2) {// 1 input files & 1 output file
         throw NUMBER_OF_ARGUMENTS_ERROR;
     }
 
-    if (!typed(argv[0], "bmp")) {
+    if (!typed(argv[START_INDEX], "bmp")) {
         throw runtime_error("Image files must be with type '.bmp'.");
     }
 
     //saves the data from the command
-    _inputFilesPath.push_back(copyToString(argv[0]));
+    _inputFilesPath.push_back(copyToString(argv[START_INDEX]));
 
+    //Calculating the _cacheString.
     if (!isSearched) {
-        if (typed(argv[1], "bmp")) {
-            _outputFilePath = copyToString(argv[1]);
+        if (typed(argv[START_INDEX], "bmp")) {
+            _outputFilePath = copyToString(argv[START_INDEX]);
         } else {
             throw runtime_error("Image files must be with type '.bmp'.");
         }
     }
 
-    _cacheString = getCacheCode() + " " + to_string(crc32(_inputFilesPath.at(0)));
+    _cacheString = getCacheCode() + " " + to_string(crc32(_inputFilesPath.at(START_INDEX)));
 }
 
 string ImageConvertOperation::getOutputFileType() const { return "bmp"; }
@@ -41,5 +44,5 @@ void ImageConvertOperation::writeToFile(const string& fileName) const {
     if(_outputFilePath.compare(NOT_INITIALIZED) == 0) {//no output file
         return;
     }
-    testing::bmp::convert_to_grayscale(_inputFilesPath.at(0), fileName);
+    testing::bmp::convert_to_grayscale(_inputFilesPath.at(START_INDEX), fileName);
 }
--- src/ImageConvertOperation.hpp
+++ src/ImageConvertOperation.hpp
@@ -4,8 +4,18 @@
 #include "Operation.hpp"
 #include "bmp_tester.hpp"
 
+/**
+ * @brief This class stands for the "image convert" operation.
+ */
 class ImageConvertOperation : public Operation {
 public:
+    /**
+     * @brief Construct a new Image Convert Operation object.
+     * 
+     * @param argc the lenght of the input (argv).
+     * @param argv includes the input files (& the output file if not from searched operation).
+     * @param isSearched true if the originall operation was search,else false.
+     */
     ImageConvertOperation(const int argc, const char *argv[], bool isSearched = false);
     
     string getOutputFileType() const override;
