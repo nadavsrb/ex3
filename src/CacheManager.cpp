0ae90092f1715ad8c0ef2ecc05845d1e151252cf
Nadavsrb <nadavsrb@gmail.com>
2020-09-04 17:17:39 +0300

    Fixing last conventions in the files

--- src/CacheManager.cpp
+++ src/CacheManager.cpp
@@ -9,7 +9,6 @@ CacheManager::CacheManager(unique_ptr<Operation>& op) {
 /**
  * @brief throws an error if the user added a file named like our cache file.
  * if the cache file does not exist the function creates a new one.
- * 
  */
 void checkCacheFileExists() {
     //make the dir cache
@@ -44,37 +43,68 @@ void checkCacheFileExists() {
     close(cachefd);
 }
 
+/**
+ * @brief Creates a Beckup File.
+ * for an operation.
+ * 
+ * @param _operation the operation to make beckup file for.
+ * @param index the index of the beckup file.
+ */
 void createBeckupFile(const Operation& _operation, unsigned int index) {
+    //gets the files name.
     string fileName = "src/bin/cache/files/" + std::to_string(index) + "." + _operation.getOutputFileType();
+
+    //opening (& creating if needed) the file.
     const auto cachefd = open(fileName.c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if (cachefd < 0) {
         throw system_error{errno, system_category()};
     }
-    _operation.writeToFile(fileName);
+
+    //try to write the beckupfile.
+    try{
+        _operation.writeToFile(fileName);
+    } catch(const exception& e) {
+        close(cachefd);
+        throw e;
+    }
+
+    close(cachefd);
 }
 
+/**
+ * @brief Get the Cash File Index for the beckup file.
+ * 
+ * @return uint32_t the index for the beckup file.
+ */
 uint32_t getCashFileIndex() {
+    //opens the catche file
     ifstream cacheFile;
     cacheFile.open(CacheManager::CACHE_FILE);
     if (cacheFile.fail()) {
         throw std::system_error(errno, system_category());
     }
 
+    //helping varible
     string line;
 
+    //if not operation was done yet
     getline(cacheFile, line); //the title
     if (cacheFile.peek() == std::ifstream::traits_type::eof()) {
         cacheFile.close();
         return 0;
     }
 
+    //gets the next index
     getline(cacheFile, line);
     cacheFile.close();
     return std::stoi(line.substr(line.find("|") + 1)) + 1;
 }
 
 void CacheManager::performOperation(bool isSearched /*= false*/, bool isClear /*= false*/) const {
+    //chcking if the catch file exists & creating it if neede.
     checkCacheFileExists();
+
+    //if clear operation
     if (isClear) {
         if (!std::filesystem::remove_all(CACHE_DIR)) {
             throw system_error(errno, system_category());
@@ -82,6 +112,7 @@ void CacheManager::performOperation(bool isSearched /*= false*/, bool isClear /*
         return;
     }
 
+    //if search operation
     if (isSearched) {
         string search = this->search();
         if (search == "") {
@@ -92,13 +123,17 @@ void CacheManager::performOperation(bool isSearched /*= false*/, bool isClear /*
         return;
     }
 
+    //searching for the operatin in the cache file. 
     string result = search();
+
+    //if found the operation
     if (result.compare("") != 0) {
         // first we will find the cache file that suits to the operation and copy it to our destination file
         string replace, fileName;
         unsigned int index = stoi(result.substr(result.find_last_of('|') + 1));
         fileName = CACHE_FILES_DIR_ + to_string(index) + '.' + _operation->getOutputFileType();
 
+        //writing the content from the beckup file to the output file.
         _operation->writeToOutputFile(readFileContent(fileName));
 
         // changing the time & date
@@ -113,6 +148,9 @@ void CacheManager::performOperation(bool isSearched /*= false*/, bool isClear /*
         return;
     }
 
+    //if the operation not in cache file.
+
+    //writing the result to the output operation.
     _operation->writeToOutputFile();
 
     // writes the operation line into the cache file
@@ -125,9 +163,13 @@ void CacheManager::performOperation(bool isSearched /*= false*/, bool isClear /*
         cache += ",";
         CurrentTime ct = CurrentTime();
         cache += ct.getTime();
+
+        //adding the beckup file.
         unsigned int index = getCashFileIndex();
         cache += "|" + std::to_string(index);
         cache += "\n";//end of line
+
+        //adding the other cache
         cache += cacheCopy;
 
         writeFileContent(CACHE_FILE, cache);
@@ -136,29 +178,35 @@ void CacheManager::performOperation(bool isSearched /*= false*/, bool isClear /*
     }
 }
 
-string CacheManager::search() const {    
+string CacheManager::search() const {   
+    //opens the catch file 
     ifstream cacheFile;
     cacheFile.open(CACHE_FILE);
     if (cacheFile.fail()) {
         throw std::system_error(errno, system_category());
     }
 
-    // checks if every begining of a line is similar to the CacheString of the operation
-    // if it finds the similar one it will return something to print
+    //exception
     string line, operationLine;
     if (_operation != nullptr) {
         operationLine = _operation->getCacheString();
     } else {
+        cacheFile.close();
         throw runtime_error(UNKNOWN_COMMAND);
     }
+
+    // checks if every begining of a line is similar to the CacheString of the operation
+    // if it finds the similar one it will return something to print
     int lastToCompare = operationLine.find_last_of(',') - 1;
     getline(cacheFile, line); //the title
     while (getline(cacheFile, line)) {
         if (line.substr(0, line.find(',')).compare(operationLine.substr(0, lastToCompare)) == 0) {
+            cacheFile.close();
             return line.substr(line.find(',') + 1);
         }
     }
 
+    cacheFile.close();
     return ""; //didn't find
 }
 
