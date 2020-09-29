712691a8fde5e256d48af47a12441dbef2bc1342
tseela <tseelagamzo@gmail.com>
2020-09-29 13:49:06 +0300

    Fixed note- func crc32FromString.

--- src/hash/crc32FileReader.cpp
+++ src/hash/crc32FileReader.cpp
@@ -14,17 +14,10 @@ uint32_t crc32::crc32FromString(const string& file) {
     //because of /0
     unsigned int length = file.length() + 1;
 
-   // declaring character array 
-    char *buffer = new char[length];
-
-    // copying the contents of the 
-    // string to char array 
-    strcpy(buffer, file.c_str());
+    auto buffer = file.c_str();
 
     //calculating crc32
-    uint32_t result = crc32::crc32((unsigned char *) buffer, length -1);
+    uint32_t result = crc32::crc32(buffer, length -1);
 
-    //deletes the buffer 
-    delete[] buffer;
     return result;
 }
